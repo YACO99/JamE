0acd0a5645da7b785fe05d25efed8525d4c6f457
YACOPC\Yaco <yyyyaaco@gmail.com>
2024-02-24 19:23:22 -0300

    net

--- Code/Net.cs
+++ Code/Net.cs
@@ -1,15 +1,48 @@
 using Godot;
 using System;
+using System.Threading;
+using Data;
+using System.Net.Sockets;
 
 public partial class Net : Node
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+	List<Client> Clients = new List<Client>();
+	Thread t1, t2;
+	bool start = false;
+	TcpListener listener;
+	int ID = 1;
+    public override void _Ready()
 	{
-	}
+		listener = new TcpListener(System.Net.IPAddress.Any,41858);
+        t1 = new Thread(new ThreadStart(delegate () { 
+			while (start)
+			{
+				Client c = new Client();
+				c.id=ID++;
+				c.tcp=listener.AcceptTcpClient();
+				c.tcp.ReceiveTimeout = 10;
+				c.tcp.GetStream().Write(BitConverter.GetBytes(c.id));
+				Clients.Add(c);
+            }
+		}));
+        t2 = new Thread(new ThreadStart(delegate () {
+            while (start)
+            {
+                
+            }
+        }));
+		start = true;
+        listener.Start();
+		t1.Start();
+    }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 	}
+}
+public class Client 
+{
+    public int id = 0;
+	public Player player;
+	public TcpClient tcp;
 }
---
+++ Data/Data/Data.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Data
+{
+    [Serializable]
+    public class Pack
+    {
+        public int id = 0;
+        public string name = "";
+        public static byte[] GetBytes(Pack p)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            MemoryStream m = new MemoryStream();
+            bf.Serialize(m, p);
+            return m.ToArray();
+        }
+
+        public static Pack SetBytes(byte[] b)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            MemoryStream m = new MemoryStream(b);
+            return (Pack)bf.Deserialize(m);
+        }
+    }
+    [Serializable]
+    public class Player : Pack
+    {
+        public int id = 0;
+        public string name = "";
+        public float px=0,py=0,pz=0,ry=0;
+    }
+}
