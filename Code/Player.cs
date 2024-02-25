09ce0e66e45f37956e1456cd598aba9f56ad7422
YACOPC\Yaco <yyyyaaco@gmail.com>
2024-02-25 09:58:40 -0300

    Player rot Y

--- Code/Player.cs
+++ Code/Player.cs
@@ -13,6 +13,7 @@ public partial class Player : CharacterBody3D
 	public override void _Ready()
 	{
 		camera = GetNode<Camera>("Camera");
+		Input.MouseMode = Input.MouseModeEnum.Captured;
 	}
 
 	public override void _PhysicsProcess(double delta)
@@ -30,10 +31,19 @@ public partial class Player : CharacterBody3D
 				velocity.Y = JumpVelocity;
 				
 			Vector2 inputDir = Input.GetVector("Left", "Right", "Forward", "Back");
-			Vector3 direction = (camera.Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+			Vector3 direction = (camera.GlobalBasis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
 			if (direction != Vector3.Zero)
 			{
-				velocity.X = direction.X * Speed;
+				var temp=(camera.GlobalRotationDegrees.Y - GlobalRotationDegrees.Y);
+				if (temp < -180)
+					temp += 360;
+                if (temp > 180)
+                    temp -= 360;
+
+                var dir = temp  * Vector3.Up * (float)delta*10;
+				GlobalRotationDegrees += dir;
+                camera.GlobalRotationDegrees -= dir;
+                velocity.X = direction.X * Speed;
 				velocity.Z = direction.Z * Speed;
 			}
 			else
